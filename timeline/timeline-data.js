6a4e2cc186f826e2ec003d064f57927b66f7d971
Wimmer <71473855+Wimmerrad@users.noreply.github.com>
2026-09-24 18:36:22 +0200

    Update timeline-data.js

--- timeline/timeline-data.js
+++ timeline/timeline-data.js
@@ -391,5 +391,183 @@ window.TIMELINE_DATA = {
       "type": "led-to",
       "note": "The Web runs on top of the internet's TCP/IP."
     }
+  ],
+  "music": [
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/01 二つの銀河.flac",
+      "size": 27045827,
+      "duration": 255.68,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "二つの銀河",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 1,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/02 水没するディンギル星.flac",
+      "size": 39812971,
+      "duration": 384.6933333333333,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "水没するディンギル星",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 2,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/03 移動要塞.flac",
+      "size": 21832974,
+      "duration": 180.72,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "移動要塞",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 3,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/04 ハイパー放射ミサイル.flac",
+      "size": 12843102,
+      "duration": 115.30666666666667,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "ハイパー放射ミサイル",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 4,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/05 大魔神.flac",
+      "size": 13692817,
+      "duration": 149.05333333333334,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "大魔神",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 5,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/06 ファイナル ヤマト.flac",
+      "size": 20473236,
+      "duration": 237.05333333333334,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "ファイナル ヤマト",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 6,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/07 悲しみ.flac",
+      "size": 25163969,
+      "duration": 269.0933333333333,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "悲しみ",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 7,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/08 超巨大戦艦ガルンボルスト.flac",
+      "size": 15887875,
+      "duration": 144.04,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "超巨大戦艦ガルンボルスト",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 8,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/09 古代(おれ)とヤマト.flac",
+      "size": 18980998,
+      "duration": 193.09333333333333,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "古代(おれ)とヤマト",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 9,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/10 薄幸のディンギル少年.flac",
+      "size": 22626846,
+      "duration": 241.90666666666667,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "薄幸のディンギル少年",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 10,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/11 二人のコスモゼロ.flac",
+      "size": 13917730,
+      "duration": 118.64,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "二人のコスモゼロ",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 11,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/12 アクエリアス レクイエム.flac",
+      "size": 19875605,
+      "duration": 214.09333333333333,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "アクエリアス レクイエム",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 12,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/13 SYMPHONY OF THE AQUARIUS.flac",
+      "size": 58137885,
+      "duration": 589.8266666666667,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "SYMPHONY OF THE AQUARIUS",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 13,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/14 ディンギル少年のテーマ.flac",
+      "size": 15918107,
+      "duration": 184.46666666666667,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "ディンギル少年のテーマ",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 14,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/15 沖田(父)と古代(子).flac",
+      "size": 28373021,
+      "duration": 272.61333333333334,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "沖田(父)と古代(子)",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 15,
+      "year": "2013"
+    },
+    {
+      "src": "music/[J-SONGS][2013.11.20][COCX-37405] YAMATO SOUND ALMANAC 1983-II Final Yamato Music Collection Part 2/16 悲愴のボレロ.flac",
+      "size": 22864050,
+      "duration": 229.88,
+      "format": "FLAC · 44.1 kHz · 16-bit · stereo",
+      "title": "悲愴のボレロ",
+      "artist": "シンフォニック・オーケストラ・ヤマト",
+      "album": "YAMATO SOUND ALMANAC1983-II「宇宙戦艦ヤマト完結編 音楽集 Part2」",
+      "track": 16,
+      "year": "2013"
+    }
   ]
 };
